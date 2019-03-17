ffc7657cc583c06278286bdb2d65f472b7b7be9d
shaktigurung <mr.shakti.gurung@gmail.com>
2019-03-17 21:12:58 +1100

    completed with some static data

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import './Tour.scss';
 
 export default class Tour extends Component {
+  state = {
+      showInfo: false
+  }
+  handleInfo = () => {
+     this.setState({
+         showInfo: !this.state.showInfo
+     });
+  }
+
   render() {
+    const { id, city, img, name, info} = this.props.tour;
+    const {removeTour} = this.props;
     return (
       <article className="tour">
         <div className="img-container">
-            <img src="https://images.pexels.com/photos/1918162/pexels-photo-1918162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"  alt="" />
-            <span className="close-btn">
+            <img src={img}  alt="" />
+            <span className="close-btn" onClick = {()=> {removeTour(id)}}>
                 <i className="fas fa-window-close" />
             </span>
         </div> 
         <div className="tour-info">
-            <h3>city</h3>
-            <h4>name</h4>
+            <h3>{city}</h3>
+            <h4>{name}</h4>
             <h5> 
                 info {" "}
-                <span>
+                <span onClick={this.handleInfo}>
                     <i className="fas fa-caret-square-down" /> 
                 </span>
             </h5>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+            {this.state.showInfo &&  <p>{info}</p>}
         </div>   
       </article>
     )
